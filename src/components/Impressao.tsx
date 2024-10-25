e983205e5f48a44d0393958a8bedf523b0390e9b
Sandra Felicio <mjpfelicia@gmail.com>
2024-10-25 09:59:52 -0300

    melhorando css do compomente de impressão

--- src/components/Impressao.tsx
+++ src/components/Impressao.tsx
@@ -4,49 +4,51 @@ import "./pagamento/Pagamento.css";
 import { Ticket } from './service/apiTicket';
 
 // Definindo as propriedades aceitas pelo componente ImpressaoTicket
-interface ImpressaoTicketProps { 
-    ticket: Ticket; 
-    ticketNumber: string; 
-    total: number; 
-    quantidade: number; 
-    formaPagamento: string; 
-    dataRetirada: string; 
-    statusPagamento: string; 
+interface ImpressaoTicketProps {
+    ticket: Ticket;
+    ticketNumber: string;
+    total: number;
+    quantidade: number;
+    formaPagamento: string;
+    dataRetirada: string;
+    statusPagamento: string;
     dataCriacao: string;
 }
 
 // Função principal do componente ImpressaoTicket
-const ImpressaoTicket: React.FC<ImpressaoTicketProps> = ({ 
-    ticket, 
-    ticketNumber, 
-    total, 
-    quantidade, 
-    formaPagamento, 
-    dataRetirada, 
-    statusPagamento, 
-    dataCriacao 
-}) => { 
+const ImpressaoTicket: React.FC<ImpressaoTicketProps> = ({
+    ticket,
+    ticketNumber,
+    total,
+    quantidade,
+    formaPagamento,
+    dataRetirada,
+    statusPagamento,
+    dataCriacao
+}) => {
     console.log("ImpressaoTicket:", ticket);
 
-    return ( 
-        <div className="impressao-ticket printableArea" id="printableArea">
+    return (
+        <div className="impressao-ticket" >
             <h3>Impressão do Ticket</h3>
-            <p><strong>Número do Ticket:</strong> {ticketNumber}</p>
-            <p><strong>Total de Peças:</strong> {quantidade}</p>
-            <p><strong>Total a Pagar:</strong> R${total.toFixed(2)}</p>
-            <p><strong>Forma de Pagamento:</strong> {formaPagamento}</p>
-            <p><strong>Status do Pagamento:</strong> {statusPagamento}</p>
-            <p><strong>Data de Criação:</strong> {dataCriacao}</p>
-            <p><strong>Data de Retirada:</strong> {dataRetirada}</p>
-            <h4>Itens:</h4>
-            <ul>
-                {ticket.items.length > 0 ? ticket.items.map((peca, idx) => ( 
-                    <li key={idx}>
-                        {peca.subTipo} ({peca.quantidade}) - valor R${peca.total.toFixed(2)}
-                    </li>
-                )) : <li>Nenhum item selecionado</li>}
-            </ul>
-            <button onClick={() => window.print()}>Imprimir Ticket</button>
+            <div id="printableArea" className='printableArea'>
+                <p><strong>Número do Ticket:</strong> {ticketNumber}</p>
+                <p><strong>Total de Peças:</strong> {quantidade}</p>
+                <p><strong>Total a Pagar:</strong> R${total.toFixed(2)}</p>
+                <p><strong>Forma de Pagamento:</strong> {formaPagamento}</p>
+                <p><strong>Status do Pagamento:</strong> {statusPagamento}</p>
+                <p><strong>Data de Criação:</strong> {dataCriacao}</p>
+                <p><strong>Data de Retirada:</strong> {dataRetirada}</p>
+                <h4>Itens:</h4>
+                <ul>
+                    {ticket.items.length > 0 ? ticket.items.map((peca, idx) => (
+                        <li key={idx}>
+                            {peca.subTipo} ({peca.quantidade}) - valor R${peca.total.toFixed(2)}
+                        </li>
+                    )) : <li>Nenhum item selecionado</li>}
+                </ul>
+            </div>
+            <button className='btnImpressao' onClick={() => window.print()}>Imprimir Ticket</button>
         </div>
     );
 };
